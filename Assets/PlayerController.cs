e07f0187591435b2629bfb20f1f8ba96d2f5dc38
feliann <101577329+feliann@users.noreply.github.com>
2022-05-26 19:35:53 -0300

    movimiento izq y der terminado

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float CarSpeed=5f;
+    Vector3 MoveInput = Vector3.zero;
+    CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        MoveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        MoveInput = transform.TransformDirection(MoveInput) * CarSpeed;
+
+        characterController.Move(MoveInput * Time.deltaTime);
+    }
+
+}
