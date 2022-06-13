77fe341a61da224856ab6daabceb40a495b5587a
feliann <101577329+feliann@users.noreply.github.com>
2022-06-13 09:59:11 -0300

    two objects added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,13 +11,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
       
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rb.AddForce(-speed * Time.deltaTime, 0,0);
+            rb.AddForce(speed * Time.deltaTime, 0,0);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            rb.AddForce(speed * Time.deltaTime, 0, 0);
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
         }
        
     }
